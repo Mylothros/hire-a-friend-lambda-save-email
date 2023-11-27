356c49bb890f99e993fa60601ec882e0d39d3412
charalampos <charalampos.mylothros@navd.ai>
2023-11-27 20:45:50 +0200

    debug

--- lambdas/lambda-save-email/app.js
+++ lambdas/lambda-save-email/app.js
@@ -58,7 +58,7 @@ exports.lambdaHandler = async (event) => {
     if (event.httpMethod !== "POST" || !event.body) {
       throw new Error("Invalid request: Expected a POST request with a non-empty body.");
     }
-    console.log("event.header.origin: ", event.header.origin, "process.env.URL: ",process.env.URL)
+    console.log("event.header.origin: ", event, "process.env.URL: ",process.env.URL)
     if(event.header.origin == process.env.URL) {
       const data = event.body;
       await upload(data);
